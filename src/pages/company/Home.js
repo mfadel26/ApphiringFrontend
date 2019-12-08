63dfcd378156b4bb19b012326ed6127abb1667ab
mfadel26 <57039555+mfadel26@users.noreply.github.com>
2019-12-08 22:55:11 +0700

    Update Home.js

--- src/pages/company/Home.js
+++ src/pages/company/Home.js
@@ -1,5 +1,6 @@
 import React, { Component} from 'react';
 import Login from '../Login'
+import Cardsen from '../../components/Cardsen';
 class Home extends Component{
     render() {
         return(
