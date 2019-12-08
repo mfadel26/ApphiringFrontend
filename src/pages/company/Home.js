66cb19bbf334488b73f6e81ba0619ecaf3d7a262
mfadel26 <muhammadfadelp@gmail.com>
2019-12-08 23:30:13 +0700

    uptodate

--- src/pages/company/Home.js
+++ src/pages/company/Home.js
@@ -1,10 +1,11 @@
 import React, { Component} from 'react';
 import Login from '../Login'
+import Cardsen from '../../components/Cardsen';
 class Home extends Component{
     render() {
         return(
             <div>
-                <Login />
+                <Cardsen />
             </div>
     );
   }
